e6cdd0f6dea31135baf1919562b264becb8f13a5
jessica <ladyjtm@gmail.com>
2015-04-21 15:44:25 -0700

    Add Facebook login

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'omniauth-facebook'
+gem "koala", "~> 1.10.0rc"
 
 group :development, :test do
   gem "dotenv-rails"
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :messages, only: [:index, :new, :create, :show]
 
-  root to: "messages#index"
+  root to: 'users#index', via: :get
+  get 'auth/facebook', as: "auth_provider"
+  get 'auth/facebook/callback', to: 'users#login'
 end
